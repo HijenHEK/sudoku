5c4bab29f6e08119f5643838447d26c10fe29983
HijenHek <k.hijen@gmail.com>
2020-09-17 09:57:44 +0100

    generator set

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <sudokuNav />
+
+  <sudokuGrid />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import sudokuGrid from './components/sudokuGrid.vue'
+import sudokuNav from './components/sudokuNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    sudokuGrid,
+    sudokuNav
   }
 }
 </script>
